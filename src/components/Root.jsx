b81a5f3a4ca9874443b38bb7bf97f7855961c2f3
Steven Inouye <steveninouye@msn.com>
2019-03-22 05:52:18 -0700

    Add main skeleton for application in Root Component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import configureStore from '../reducers/root_reducer';
+import CurrentWeatherContainer from './current_weather/CurrentWeatherContainer';
+import ZipInputContainer from './zip_input/ZipInputContainer';
+import ForecastWeatherContainer from './forcast_weather/ForecastWeatherContainer';
+
 const Root = () => (
   <Provider store={configureStore()}>
     <div id="weather-app">
-weather app
+      <div className="left-container">
+        <h1 className="current-header">Current</h1>
+        <div className="current">
+          <CurrentWeatherContainer />
+        </div>
+        <ZipInputContainer />
+      </div>
+      <div className="right-container">
+        <h1 className="forecast-header">Forecast</h1>
+        <div className="forecast">
+          <ForecastWeatherContainer />
+        </div>
+      </div>
     </div>
   </Provider>
 );
